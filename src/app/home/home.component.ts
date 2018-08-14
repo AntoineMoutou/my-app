95d336592c15e6d39c1848e673b6bbdbeb28e3bb
Antoine Moutou <moutou.antoine@hotmail.fr>
2018-08-14 13:14:03 +0200

    proto-three met proto-babylon

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-	title = '3D module prototype';
+	title = '3D module prototypes';
 
   constructor() { }
 
